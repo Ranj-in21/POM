7e7eb7390ab2240c2b738c877a9b714a3f57b6dd
Ranj-in21 <108992272+Ranj-in21@users.noreply.github.com>
2022-08-27 20:31:46 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,34 @@
+package testCase;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import base.ProjectSpecificationMethod;
+import pages.LoginPage;
+
+public class CreateLead extends ProjectSpecificationMethod{
+	
+	@BeforeTest
+	
+	public void setFile() {
+		excelFile = "CreateLead";
+		
+	}
+	
+		@Test(dataProvider ="TestData")
+		public void runCreateLead(String UserName, String Password, String Company, 
+				String FirstName, String LastName, String PhoneNumber) {
+			
+			LoginPage lp = new LoginPage(driver);
+			System.out.println(driver);
+			lp.enterUserName(UserName).enterPassword(Password).clickLogin()
+			.clickCrmsfa()
+			.clickLeads()
+			.clickCreateLead()
+			.companyName(Company).firstName(FirstName).lastName(LastName).phoneNumber(PhoneNumber)
+			.clickCreateLeadButton().verifyLead(FirstName);
+			
+		}
+	
+
+}
---
+++ LoginTest.java
@@ -0,0 +1,27 @@
+package testCase;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import base.ProjectSpecificationMethod;
+import pages.LoginPage;
+
+public class LoginTest extends ProjectSpecificationMethod {
+	
+		@BeforeTest
+	
+	public void setFile() {
+		excelFile = "Login";
+		
+	}
+	
+		@Test(dataProvider = "TestData")
+	public void runLogin(String UserName, String Password) {
+		
+		LoginPage lp = new LoginPage(driver);
+		lp.enterUserName(UserName).enterPassword(Password).clickLogin();
+		
+
+	}
+
+}
